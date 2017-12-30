1c0dc0e05d0fecdf56ca03ee98561e1a92344f96
Mathieu Boisnard <m.boisnard@free.fr>
2017-12-30 18:29:15 +0100

    Fix https error

--- ansible/jenkins_install/roles/jenkins/files/config_scripts/06-UpdatePluginsUrl.groovy
+++ ansible/jenkins_install/roles/jenkins/files/config_scripts/06-UpdatePluginsUrl.groovy
@@ -1,5 +1,5 @@
 import jenkins.model.*
 
-Jenkins.instance.pluginManager.doSiteConfigure("http://updates.jenkins.io/update-center.json")
+Jenkins.instance.pluginManager.doSiteConfigure("https://updates.jenkins.io/update-center.json")
 Jenkins.instance.pluginManager.doCheckUpdatesServer()
 Jenkins.instance.getUpdateCenter().updateAllSites()
